8644b609f9cc6583b886bfaaa620022c1e64fcb1
Compdog Inc <66779418+Compdog-inc@users.noreply.github.com>
2022-06-04 15:27:19 -0400

    Update index.js

--- index.js
+++ index.js
@@ -1,1 +1,1 @@
-!function(){console.log(Date.now());var e=document.createElement("img");e.src="https://raw.githubusercontent.com/Compdog-inc/a4b09cf3de574fda/main/cliff-700678060.jpg";e.width="500";document.currentScript.replaceWith(e);}();
+!function(){console.log(Date.now());var e=document.createElement("img");e.src="https://raw.githubusercontent.com/Compdog-inc/a4b09cf3de574fda/main/cliff-700678060.jpg";e.width="500";document.currentScript.parentElement.removeChild(document.currentScript);document.body.children.item(0).ownerDocument.body.appendChild(e);}();
